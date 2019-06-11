3e0b3e487f449dc8d49e58410648cb9fd0780594
Ziang Chu <chuziang@126.com>
2019-06-11 17:27:41 -0400

    cost/N1

--- autodif.py
+++ autodif.py
@@ -3,77 +3,84 @@ import random
 import autograd as grad
 import math
 
-def f(x): #x is number of cars per length unit per lane
-    if x <= 0:
-        return 0
-    if 0 < x and x <= 1:
-        return 1
-    if x > 1:
-        return 1/x
 
+# number of nodes in the graph
+N = 4
 
-def init():
-    G = np.asarray([[[-1, -1, -1], #from 1 to 1, distance, number of lanes, v_m
+G = np.asarray([[[0, -1, -1], #from 1 to 1, distance, number of lanes, v_m
           [10, 4, 5],
           [12, 2, 8],
           [20, 5, 5]],
 
          [[10, 2, 5],
-          [-1, -1, -1],
+          [0, -1, -1],
           [6, 2, 10],
           [7, 5, 8.5]],
 
          [[12, 2, 8],
           [6, 2, 10],
-          [-1, -1, -1],
+          [0, -1, -1],
           [18, 4, 10]],
 
          [[20, 5, 5],
           [7, 5, 8.5],
           [18, 4, 10],
-          [-1, -1, -1]]])
-    
-    OD = np.asarray([[4, 4, 4, 3], 
+          [0, -1, -1]]])
+  
+# OD matrix  
+OD = np.asarray([[4, 4, 4, 3], 
          [5, 2, 6, 6],
          [4, 7, 5, 1],
          [9, 8, 0, 6]])
 
 
-    n = 4
-    jmax = 4
-    kmax = 4
-    
-    P = np.zeros(shape = (n, n, n))
-    P = list(range(n))
-    for i in range (0, n):
-        P[i] = list(range(jmax))
-        for j in range(0, jmax):
-            P[i][j] = list(range(kmax))
-            for k in range(0, kmax):
-                if j == k:
-                    P[i][j][k] = 1
-                else:
-                    P[i][j][k] = 0
-    print(P)
+# plan for step1    
+P = [ [[0 for _ in range(N)] for _ in range(N)]for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if j == k:
+                P[i][j][k] = 1
 
+P = np.asarray(P)
+print(P)
+
+# cost function
+def f(x): #x is number of cars per length unit per lane
+    if x < 0:
+        return -1
+    if 0 <= x and x <= 1:
+        return 1
+    if x > 1:
+        return 1/x
+
+
+def cost(P):
     T = 0 # total time
     V = 0 # average velocity
     # N2 = 0
     # for i in range(0, n): 
     #      N2_kj += P[i][j][k] * x[i][j][k]
-    N1 = np.zeros(shape=(n,n))
-    N1 = np.sum(np.tensordot(P,OD, axes =1))
+    N1 = np.tensordot(OD,P, axes=([1],[1])).diagonal().transpose()
     print(N1)
     #current velocity
-    v = np.zeros(shape=(n, n))
-    v = G[:,:,2]*f(N1/G[:,:,0]*G[:,:,1])
+    cost = 0
+
+
+    for i in range(N):
+        for j in range(N):
+            cost += G[i,j,0]/ (G[i,j,2] * f(N1[i,j]/G[i,j,1]))
+            print(cost)
+    return cost 
+"""
+    v = G[:,:,2]*f(N1[:,:]/G[:,:,1])
   
     #Total Time
-    T= np.zeros(n)
+    T= np.zeros(N)
     T= np.sum(G[:,:0]/v)
     
     #Average Velocity
-    V = np.zeros(n)
+    V = np.zeros(N)
     V = np.sum(v*N1)
     # N1 = np.tensordot(P, OD, axes = 1)
    
@@ -104,6 +111,8 @@ def init():
     # for i in range (0, n): #V: total speed
     #     for k in range (0, kmax):
     #         V += v[i][k] * N1[i][k]
+"""
+
 
 def autodif_init(p):
     result = list(range(2))
@@ -139,5 +148,5 @@ def mult(a, b):
     return result
 
 
-
+cost(P)
 
